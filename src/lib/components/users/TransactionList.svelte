ba700b0f745c10d18ae3ca28155a088867b7e414
OmentaElvis <omentaelvis@gmail.com>
2025-08-09 00:36:21 +0300

    Fixed margin and dead code
    Signed-off-by: OmentaElvis <omentaelvis@gmail.com>

--- src/lib/components/users/TransactionList.svelte
+++ src/lib/components/users/TransactionList.svelte
@@ -29,34 +29,6 @@
     }
   }
 
-  function getMpesaMessage(transaction: FullTransactionLog): string {
-    if (transaction.status !== TransactionStatus.Completed) {
-      return `Transaction ${transaction.status}.`;
-    }
-
-    const amount = formatTransactionAmount(transaction.transaction_amount);
-    const new_balance = formatTransactionAmount(transaction.new_balance);
-    const date = formatTransactionDate(transaction.transaction_date);
-    const time = new Date(transaction.transaction_date).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false });
-    const transactionId = transaction.transaction_id;
-    const cost = formatTransactionAmount(transaction.fee);
-
-    switch (transaction.transaction_type) {
-      case TransactionType.SendMoney:
-        return `<b>${transaction.transaction_id}</b> Confirmed. <b>${amount}</b> sent to <button onclick={viewDetails}><b>${transaction.to_name}</b></button> on ${date} at ${time}. New M-PESA balance is ${new_balance}. Transaction cost, ${cost} `;
-      case TransactionType.Deposit:
-        return `<b>${transaction.transaction_id}</b> Confirmed. Deposit <b>${amount}</b> on ${date} at ${time}. New M-PESA balance is ${new_balance}.`;
-      case TransactionType.Paybill:
-        return `<b>${transaction.transaction_id}</b> Confirmed. <b>${amount}</b> paid to Pay Bill <button onclick={viewDetails}><b>${transaction.to_name}</b></button> for account ${transaction.from_name || 'N/A'} on ${date} at ${time}. New M-PESA balance is ${new_balance}. Transaction cost, ${cost} `;
-      case TransactionType.BuyGoods:
-        return `<b>${transaction.transaction_id}</b> Confirmed. <b>${amount}</b> paid to Buy Goods <button onclick={viewDetails}><b>${transaction.to_name}</b></button> on ${date} at ${time}. New M-PESA balance is ${new_balance}. Transaction cost, ${cost} `;
-      case TransactionType.Withdraw:
-        return `<b>${transaction.transaction_id}</b> Confirmed. <b>${amount}</b> withdrawn at ${transaction.from_name || 'Agent'} on ${date} at ${time}. New M-PESA balance is ${new_balance}. Transaction cost, ${cost} `;
-      default:
-        return `${transaction.transaction_id} Confirmed. <b>${amount}</b> transaction on ${date} at ${time}. Transaction ID: ${transactionId}. Transaction cost, ${cost}`;
-    }
-  }
-
   function formatValues(transaction: FullTransactionLog) {
     const amount = formatTransactionAmount(transaction.transaction_amount);
     const new_balance = formatTransactionAmount(transaction.new_balance);
@@ -78,7 +50,7 @@
   }
 </script>
 
-<div class="h-full overflow-auto m-4">
+<div class="h-full overflow-auto mx-4">
     <ScrollArea class="h-full">
       {#each transactions as transaction}
         <div class="m-4 flex {isSentTransaction(transaction) ? 'justify-end' : 'justify-start'}">
