a2c2d6264904d61093b7644cddcf8af93b6380a4
OmentaElvis <omentaelvis@gmail.com>
2025-12-22 08:13:29 +0300

    chore(ci): update tauri version in crates/pesa-tauri/tauri.config.json

--- .github/workflows/prepare-release.sh
+++ .github/workflows/prepare-release.sh
@@ -7,4 +7,9 @@ echo "Updating root Cargo.toml to version ${NEW_VERSION}..."
 sed -i "s/\(version = \"\)[0-9.]*\(\"\)/\1${NEW_VERSION}\2/" Cargo.toml
 echo "Root Cargo.toml updated."
 
+echo "Update tauri config"
+tmp="$(mktemp)"
+jq ".version = \"$(NEW_VERSION)\"" crates/pesa-tauri/tauri.conf.json > "$tmp" \
+  && mv "$tmp" crates/pesa-tauri/tauri.conf.json
+
 echo "Prepare step for semantic-release completed."
